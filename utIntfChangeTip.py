4a41a315a03f1ae104e553b9de37945db2a42710
James Morgan <james.g.morgan@gmail.com>
2014-08-03 02:48:47 -0400

    working

---
+++ utIntfChangeTip.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+"""
+2014-08-02 JGM Created.
+"""
+
+import datetime
+import pprint
+
+
+class UtIntfChangeTip(object):
+    def __init__(self):
+        self.init()
+        pass
+    
+    def init(self):
+        pass
+    
+    def getTipText(self, user, msg, quantity, token):
+        bRet = True
+        tipText = ""
+        
+        return bRet, tipText
+    
+def main():
+    ut_intf_changetip = UtIntfChangeTip()
+    pass
+
+if  __name__ == "__main__":
+    main()
+
+    
+

2610a5c762437a412f6d6f12b6461d02f032bb28
James Morgan <james.g.morgan@gmail.com>
2014-09-26 21:25:47 -0400

    Added followers() method and well, PyCharm ;-)

--- utIntfTwitter.py
+++ utIntfTwitter.py
@@ -48,15 +48,39 @@ class UtIntfTwitter(object):
                 text = stat['text'].encode('unicode-escape')
                 line = "%s %s %s" % (created, user, text)
                 print line
-            #print json.dumps(res)
-        return True 
-    
+        return True
+
+    def followers(self):
+        def process(scr_names):
+            res = self.__api_Twit.users.lookup(screen_name=','.join(scr_names), _timeout=1)
+            for item in res:
+                loc = item['location'].lower()
+                if "dominica" in loc:
+                    print "%-20s %-30s %s" % (item['screen_name'], item['location'], item['description'])
+                    #import pprint
+                    #pprint.pprint(item)
+
+        import json
+        #pprint.pprint(self.__api_Twit.statuses.user_timeline(screen_name="changetip", count=1))
+        with open(r'C:\Dropbox\Proj\GitHub\v3ndr01d\followers.txt') as f:
+            scrname_list = f.read().splitlines()
+        n_total = len(scrname_list)
+        n_hundreds = n_total/100
+        for i in range(0, n_hundreds):
+            r_lower = i*100
+            r_upper = r_lower + 99
+            #print "%d" % (r_upper)
+            sub_list = scrname_list[r_lower:r_upper]
+            process(sub_list)
+        r_lower = n_hundreds*100
+        r_upper = r_lower + n_total - r_lower
+        sub_list = scrname_list[r_lower:r_upper]
+        process(sub_list)
+
+
 def main():
     ut_intf_twitter = UtIntfTwitter(None)
-    bRet = ut_intf_twitter.exClassMethod()
+    bRet = ut_intf_twitter.followers()
 
-if  __name__ == "__main__":
+if __name__ == "__main__":
     main()
-
-    
-
